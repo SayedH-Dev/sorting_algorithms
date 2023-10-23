4d454ad1112f8a1600df5c60e8c323889edbeac4
Saraali24 <saramabrouk.ali@gmail.com>
2023-10-23 07:35:57 -0700

    sorting_algorthim

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
+	size_t i, j;
+	size_t minValue;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		min_index = i;
+		minValue = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[minValue])
+				minValue = j;
 		}
-		if (min_index != i)
+		if (minValue != i)
 		{
 			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
+			array[i] = array[minValue];
+			array[minValue] = temp;
 
 			print_array(array, size);
 		}
